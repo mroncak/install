df490a54e6e3d3e556f9efbc507c49272357b1d2
mroncak <roncak@gmail.com>
2023-09-21 13:26:27 +0200

    first

---
+++ 1-install.sh
@@ -0,0 +1,259 @@
+#/bin/bash
+#  ___           _        _ _  
+# |_ _|_ __  ___| |_ __ _| | | 
+#  | || '_ \/ __| __/ _` | | | 
+#  | || | | \__ \ || (_| | | | 
+# |___|_| |_|___/\__\__,_|_|_| 
+#                              
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+# Install Script for required packages
+# ------------------------------------------------------
+
+# ------------------------------------------------------
+# Load Library
+# ------------------------------------------------------
+source $(dirname "$0")/scripts/library.sh
+clear
+echo "  ___           _        _ _  "
+echo " |_ _|_ __  ___| |_ __ _| | | "
+echo "  | ||  _ \/ __| __/ _  | | | "
+echo "  | || | | \__ \ || (_| | | | "
+echo " |___|_| |_|___/\__\__,_|_|_| "
+echo "                              "
+echo "by Stephan Raabe (2023)"
+echo "-------------------------------------"
+echo ""
+
+# ------------------------------------------------------
+# Check if yay is installed
+# ------------------------------------------------------
+#if sudo pacman -Qs yay > /dev/null ; then
+#    echo "yay is installed. You can proceed with the installation"
+#else
+#    echo "yay is not installed. Will be installed now!"
+#    git clone https://aur.archlinux.org/yay-git.git ~/yay-git
+#    cd ~/yay-git
+#    makepkg -si
+#    cd ~/dotfiles/
+#    clear
+#    echo "yay has been installed successfully."
+#    echo ""
+#    echo "  ___           _        _ _  "
+#    echo " |_ _|_ __  ___| |_ __ _| | | "
+#    echo "  | ||  _ \/ __| __/ _  | | | "
+#    echo "  | || | | \__ \ || (_| | | | "
+#    echo " |___|_| |_|___/\__\__,_|_|_| "
+#    echo "                              "
+#    echo "by Stephan Raabe (2023)"
+#    echo "-------------------------------------"
+#    echo ""
+#fi
+
+# ------------------------------------------------------
+# Confirm Start
+# ------------------------------------------------------
+
+while true; do
+    read -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* ) 
+            exit;
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# ------------------------------------------------------
+# Install Arco Repositories
+# ------------------------------------------------------
+sudo pacman -S wget --noconfirm --needed
+
+echo "Getting the ArcoLinux keys from the ArcoLinux repo"
+
+sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-20251209-3-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
+sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
+
+echo "Getting the latest arcolinux mirrors file"
+
+sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-23.06-01-any.pkg.tar.zst -O /tmp/arcolinux-mirrorlist-git-23.06-01-any.pkg.tar.zst
+sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-23.06-01-any.pkg.tar.zst
+echo '
+
+#[arcolinux_repo_testing]
+#SigLevel = PackageRequired DatabaseNever
+#Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo]
+SigLevel = PackageRequired DatabaseNever
+Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo_3party]
+SigLevel = PackageRequired DatabaseNever
+Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo_xlarge]
+SigLevel = PackageRequired DatabaseNever
+Include = /etc/pacman.d/arcolinux-mirrorlist' | sudo tee --append /etc/pacman.conf
+
+
+# ------------------------------------------------------
+# Install required packages
+# ------------------------------------------------------
+echo ""
+echo "-> Install main packages"
+
+packagesPacman=(
+    "pacman-contrib"
+    "alacritty" 
+    "rofi" 
+    "chromium" 
+    "nitrogen" 
+    "dunst" 
+    "starship"
+    "neovim" 
+    "mpv" 
+    "freerdp" 
+    "xfce4-power-manager" 
+    "thunar" 
+    "mousepad" 
+    "ttf-font-awesome" 
+    "ttf-fira-sans" 
+    "ttf-fira-code" 
+    "ttf-firacode-nerd" 
+    "figlet" 
+    "lxappearance" 
+    "breeze" 
+    "breeze-gtk" 
+    "vlc" 
+    "exa" 
+    "python-pip" 
+    "python-psutil" 
+    "python-rich" 
+    "python-click" 
+    "xdg-desktop-portal-gtk"
+    "pavucontrol" 
+    "tumbler" 
+    "xautolock" 
+    "blueman"
+    "arcolinux-app-glade-git"
+    "onlyoffice-bin"
+    "virt-manager"
+    "qemu-desktop"
+    "qemu-emulators-full"
+    "firewalld"
+    "ntfs-3g"
+    "brave-bin"
+    "freerdp"
+    "keepassxc"
+    "nextcloud-client"
+    "openfortivpn"
+    "timeshift"
+    "timeshift-autosnap"
+    "transmission-gtk"
+    "ranger"
+    "kdeconnect"
+    "gnome-calculator"
+    "spotifywm-git"
+    "bibata-cursor-theme" 
+    "trizen"
+    "pfetch"
+    "yay"
+);
+
+#packagesYay=(
+#    "pfetch" 
+#    "bibata-cursor-theme" 
+#    "trizen"
+#);
+  
+# ------------------------------------------------------
+# Install required packages
+# ------------------------------------------------------
+_installPackagesPacman "${packagesPacman[@]}";
+#_installPackagesYay "${packagesYay[@]}";
+
+# ------------------------------------------------------
+# Install pywal
+# ------------------------------------------------------
+if [ -f /usr/bin/wal ]; then
+    echo "pywal already installed."
+else
+    yay --noconfirm -S pywal
+fi
+
+clear
+
+# ------------------------------------------------------
+# Install .bashrc
+# ------------------------------------------------------
+echo ""
+echo "-> Install .bashrc"
+
+_installSymLink .bashrc ~/.bashrc ~/dotfiles/.bashrc ~/.bashrc
+
+# ------------------------------------------------------
+# Install custom issue (login prompt)
+# ------------------------------------------------------
+echo ""
+echo "-> Install login screen"
+while true; do
+    read -p "Do you want to install the custom login promt? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            sudo cp ~/dotfiles/login/issue /etc/issue
+            echo "Login promt installed."
+        break;;
+        [Nn]* ) 
+            echo "Custom login promt skipped."
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# ------------------------------------------------------
+# Install wallpapers
+# ------------------------------------------------------
+echo ""
+echo "-> Install wallapers"
+while true; do
+    read -p "Do you want to clone the wallpapers? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            if [ -d ~/wallpaper/ ]; then
+                echo "wallpaper folder already exists."
+            else
+                git clone https://gitlab.com/stephan-raabe/wallpaper.git ~/wallpaper
+                echo "wallpaper installed."
+            fi
+            echo "Wallpaper installed."
+        break;;
+        [Nn]* ) 
+            if [ -d ~/wallpaper/ ]; then
+                echo "wallpaper folder already exists."
+            else
+                mkdir ~/wallpaper
+            fi
+            cp ~/dotfiles/default.jpg ~/wallpaper
+            echo "Default wallpaper installed."
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# ------------------------------------------------------
+# Init pywal
+# ------------------------------------------------------
+echo ""
+echo "-> Init pywal"
+wal -i ~/dotfiles/default.jpg
+echo "pywal initiated."
+
+# ------------------------------------------------------
+# DONE
+# ------------------------------------------------------
+clear
+echo "DONE!"

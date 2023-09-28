8cefae5707de71368cb9a80f4a9e96625044768f
mroncak <roncak@gmail.com>
2023-09-28 13:26:13 +0200

    bash and fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,51 @@
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
-PS1='[\u@\h \W]\$ '
+# -----------------------------------------------------
+# ADDING TO THE PATH
+# -----------------------------------------------------
+
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.bin  $HOME/.local/bin $HOME/.config/emacs/bin $HOME/Applications /var/lib/flatpak/exports/bin/ $fish_user_paths
+
+# -----------------------------------------------------
+# EXPORT
+# -----------------------------------------------------
+
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+set EDITOR "emacsclient -t -a ''"                 # $EDITOR use Emacs in terminal
+set VISUAL "emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
+
+# -----------------------------------------------------
+# SET MANPAGER
+# -----------------------------------------------------
+
+### Uncomment only one of these!
+
+### "nvim" as manpager
+set -x MANPAGER "nvim +Man!"
+
+### "less" as manpager
+# set -x MANPAGER "less"
+
+# -----------------------------------------------------
+# SET EITHER DEFAULT EMACS MODE OR VI MODE
+# -----------------------------------------------------
+
+function fish_user_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
+end
+
+# -----------------------------------------------------
+# AUTOCOMPLETE AND HIGHLIGHT COLORS
+# -----------------------------------------------------
+
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
 
 # -----------------------------------------------------
 # ALIASES
@@ -85,20 +130,21 @@ alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
 # bare git repo alias for dotfiles
 alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 
-export PATH="/usr/lib/ccache/bin/:$PATH"
+# -----------------------------------------------------
+# SETTING THE STARSHIP PROMPT
+# -----------------------------------------------------
 
-# -----------------------------------------------------
-# START STARSHIP
-# -----------------------------------------------------
-eval "$(starship init bash)"
+starship init fish | source
 
 # -----------------------------------------------------
 # PYWAL
 # -----------------------------------------------------
+
 cat ~/.cache/wal/sequences
 
 # -----------------------------------------------------
 # PFETCH
 # -----------------------------------------------------
+
 echo ""
 pfetch

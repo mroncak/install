f9e66a519af0a4382348054cab370e66f937256b
mroncak <roncak@gmail.com>
2023-11-07 13:29:21 +0100

    update_hosts.sh

--- scripts/hosts/update_hosts.sh
+++ scripts/hosts/update_hosts.sh
@@ -2,7 +2,7 @@
 
 scp mroncak@192.168.50.50:/opt/pihole/etc-pihole/custom.list /home/mroncak/install/scripts/hosts/pihole.list
 sed -i -ne '/# Pihole Block Start/ {p; r /home/mroncak/install/scripts/hosts/pihole.list' -e ':a; n; /Pihole Block End/ {p; b}; ba}; p' /home/mroncak/install/scripts/hosts/ansible.list
-if [ -z 'grep "BEGIN\|END MANAGED BLOCK" /etc/hosts' ]
+if [ -z `grep "BEGIN\|END MANAGED BLOCK" /etc/hosts` ]
 then
 	echo '
 

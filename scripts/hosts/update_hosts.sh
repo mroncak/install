f5a155bde03fee5dce068e90d58892d63f200c9a
mroncak <roncak@gmail.com>
2023-11-07 11:30:46 +0100

    hosts scrip updated

--- scripts/hosts/update_hosts.sh
+++ scripts/hosts/update_hosts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp mroncak@192.168.50.50:/opt/pihole/etc-pihole/custom.list /home/mroncak/Script/hosts/pihole.list
-sed -i -ne '/# Pihole Block Start/ {p; r /home/mroncak/Script/hosts/pihole.list' -e ':a; n; /Pihole Block End/ {p; b}; ba}; p' /home/mroncak/Script/hosts/ansible.list
-sudo sed -i -ne '/# BEGIN ANSIBLE MANAGED BLOCK/ {p; r /home/mroncak/Script/hosts/ansible.list' -e ':a; n; /# END ANSIBLE MANAGED BLOCK/ {p; b}; ba}; p' /etc/hosts
+scp mroncak@192.168.50.50:/opt/pihole/etc-pihole/custom.list /home/mroncak/install/scripts/hosts/pihole.list
+sed -i -ne '/# Pihole Block Start/ {p; r /home/mroncak/install/scripts/hosts/pihole.list' -e ':a; n; /Pihole Block End/ {p; b}; ba}; p' /home/mroncak/install/scripts/hosts/ansible.list
+sudo sed -i -ne '/# BEGIN ANSIBLE MANAGED BLOCK/ {p; r /home/mroncak/install/scripts/hosts/ansible.list' -e ':a; n; /# END ANSIBLE MANAGED BLOCK/ {p; b}; ba}; p' /etc/hosts
